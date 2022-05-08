057ace77c4d32af932b46bdeebbe7aec8a907ee5
Panda Data Systems LLC <imitchell.main@gmail.com>
2022-05-07 22:57:06 -0700

    Final Spotify Build

---
+++ atoms/playlistAtom.js
@@ -0,0 +1,11 @@
+import { atom } from 'recoil'
+
+export const playlistState = atom({
+  key: 'playlistState',
+  default: 'null',
+})
+
+export const playistIdState = atom({
+  key: 'playlistIdState',
+  default: '7BUmWUZMCKL5wJIAPW0lSr',
+})
